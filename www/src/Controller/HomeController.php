2f8cab22710f0d5dac20f85c9bf32c01957631dd
Arthurdev02 <arthur.dev.taf@gmail.com>
2025-02-10 16:41:46 +0100

    [S] Soir Push du soir, avec rien de finis. A reprendre chez moi ou demain Important

--- www/src/Controller/HomeController.php
+++ www/src/Controller/HomeController.php
@@ -13,4 +13,21 @@ class HomeController extends AbstractController
     {
         return $this->render('base.html.twig');
     }
+    #[Route('/registration', name: 'registration')]
+    public function registration(): Response
+    {
+        return $this->render('registration/register.html.twig');
+    }
+    #[Route('/acceuil', name: 'acceuil')]
+    public function acceuil(): Response
+    {
+        return $this->render('home/index.html.twig');
+    }
+    #[Route('/reservation', name: 'reservation')]
+    public function reservation(): Response
+    {
+        return $this->render('home/index.html.twig');
+    }
 }
+
+
